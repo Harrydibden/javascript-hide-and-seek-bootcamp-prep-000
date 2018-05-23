3f5c19fe3be1e6845069b83e83c917e7192401de
Harry Dibden <harry-dibden@hotmail.com>
2018-05-23 15:09:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget(){
 function increaseRankBy(n){
   let lis = document.querySelectorAll("ul.ranked-list li");
   for(let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (lis[i].innerHTML + n).toString();
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
   }
 }
